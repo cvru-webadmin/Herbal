1b26cee9cc3a957e9a4042428da3e3dfb018fda1
Aayush Rathore <aayu.r.2003@gmail.com>
2025-02-01 16:15:13 +0530

    ✅ Ready for deployment

--- frontend/src/component/const/navbar.tsx
+++ frontend/src/component/const/navbar.tsx
@@ -5,7 +5,6 @@ import { useLogout } from "../../api/auth.api";
 
 const Navbar = () => {
   const user = userStore((state) => state.user);
-  const removeUser = userStore((state) => state.removeUser);
   const { mutate: logout } = useLogout();
   return (
     <header className="px-2 py-2 bg-green-700 sticky top-0 flex justify-between items-center text-white flex-wrap z-50">
